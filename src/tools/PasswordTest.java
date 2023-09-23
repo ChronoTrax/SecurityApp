f796e3df19caf32191c44d28f268fd9a8bb9d9af
Durval Ledo <55108934+ChronoTrax@users.noreply.github.com>
2023-09-22 22:27:25 -0400

    Password check now checks for numbers.

--- src/tools/PasswordTest.java
+++ src/tools/PasswordTest.java
@@ -1,13 +1,31 @@
 package tools;
 
+import java.util.Arrays;
+
 public class PasswordTest {
     public static String isPasswordStrong(String password) {
+        // check length
         if (password.length() < 8) {
-            return "WEAK PASSWORD: password should be at least 8 characters long.";
+            return "WEAK PASSWORD: Password should be at least 8 characters long.";
         }
 
+        // check for upper and lower case
         if (password.toLowerCase().equals(password) || password.toUpperCase().equals(password)) {
-            return "WEAK PASSWORD: password should contain upper and lower case characters.";
+            return "WEAK PASSWORD: Password should contain upper and lower case characters.";
+        }
+
+        // check for numbers
+        {
+            boolean pass = false;
+            for (Character c : Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')) {
+                if (password.contains(String.valueOf(c))) {
+                    pass = true;
+                    break;
+                }
+            }
+            if (!pass) {
+                return "WEAK PASSWORD: Password should contain a number.";
+            }
         }
 
         return "STRONG PASSWORD.";
