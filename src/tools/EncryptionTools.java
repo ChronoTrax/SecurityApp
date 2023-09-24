5a10ec3a9e39f1135f2c8b85609705d3ed6f789e
Jon Marien <nuclearsmilz@gmail.com>
2023-09-24 09:57:19 -0400

    Changed the encryption and decryption functions to return a char[] instead of a String.

--- src/tools/EncryptionTools.java
+++ src/tools/EncryptionTools.java
@@ -4,6 +4,7 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.spec.KeySpec;
 import java.util.Base64;
 
@@ -20,15 +21,17 @@ public class EncryptionTools {
      * @return
      * @throws Exception
      */
-    public static String encryptPassword(String pass, String salt) throws Exception {
-        KeySpec spec = new PBEKeySpec(pass.toCharArray(), salt.getBytes(), ITERATIONS, KEY_LENGTH);
+    public static String encryptPassword(char[] pass, String salt) throws Exception {
+        // Convert char[] password to byte[] for later use
+        byte[] passwordBytes = new String(pass).getBytes();
+
+        KeySpec spec = new PBEKeySpec(pass, salt.getBytes(), ITERATIONS, KEY_LENGTH);
         SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_ALGORITHM);
         SecretKey key = factory.generateSecret(spec);
 
         Cipher cipher = Cipher.getInstance(ENCRYTPION_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedPassword = cipher.doFinal(pass.getBytes());
-
+        byte[] encryptedPassword = cipher.doFinal(passwordBytes);
         return Base64.getEncoder().encodeToString(encryptedPassword);
     }
 
@@ -39,7 +42,7 @@ public class EncryptionTools {
      * @return
      * @throws Exception
      */
-    public static String decryptPassword(String encryptedPass, String salt) throws Exception {
+    public static char[] decryptPassword(String encryptedPass, String salt) throws Exception {
         KeySpec spec = new PBEKeySpec(encryptedPass.toCharArray(), salt.getBytes(), ITERATIONS, KEY_LENGTH);
         SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_ALGORITHM);
         SecretKey key = factory.generateSecret(spec);
@@ -48,7 +51,7 @@ public class EncryptionTools {
         cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decryptedPasswordBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedPass));
 
-        return new String(decryptedPasswordBytes);
+        // Convert byte[] decrypted password to char[]
+        return new String(decryptedPasswordBytes).toCharArray();
      }
-
 }
