714c84ae404315e321c94bd6745efa1609c6b2d5
Jon Marien <nuclearsmilz@gmail.com>
2023-09-24 11:24:24 -0400

    Changed variable and methods name for better readability.

--- src/tools/EncryptionTools.java
+++ src/tools/EncryptionTools.java
@@ -16,22 +16,24 @@ public class EncryptionTools {
     /**
      *
      * @param  masterPass Master password of user
-     * @param pass Password for current site
+     * @param userPass Password for current site
      * @param salt A randomly generated salt
      * @return char[]
      * @throws Exception could be illegal block size, padding, or {@link java.security.NoSuchAlgorithmException}
      */
-    public static String encryptPassword(char[] masterPass, char[] pass, byte[] salt) throws Exception {
+    public static String encryptUserPassword(char[] masterPass, char[] userPass, byte[] salt) throws Exception {
         // Convert char[] password to byte[] for later use
-        byte[] passwordBytes = new String(pass).getBytes();
+        byte[] masterPasswordBytes = new String(masterPass).getBytes();
+        byte[] userPasswordBytes = new String(userPass).getBytes();
 
-        KeySpec spec = new PBEKeySpec(masterPass, salt, ITERATIONS, KEY_LENGTH);
+        KeySpec masterKeySpec = new PBEKeySpec(masterPass, salt, ITERATIONS, KEY_LENGTH);
         SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_ALGORITHM);
-        SecretKey key = factory.generateSecret(spec);
+        SecretKey masterKey = factory.generateSecret(masterKeySpec);
 
         Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedPassword = cipher.doFinal(passwordBytes);
+        cipher.init(Cipher.ENCRYPT_MODE, masterKey);
+
+        byte[] encryptedPassword = cipher.doFinal(userPasswordBytes);
 
         return Base64.getEncoder().encodeToString(encryptedPassword);
     }
@@ -39,22 +41,23 @@ public class EncryptionTools {
     /**
      *
      * @param masterPass Master password for user
-     * @param encryptedPass Password for current site
+     * @param encryptedUserPass Password for current site
      * @param salt A randomly generated salt
      * @return char[]
      * @throws Exception could be illegal block size, padding, or {@link java.security.NoSuchAlgorithmException}
      */
-    public static char[] decryptPassword(char[] masterPass, String encryptedPass, byte[] salt) throws Exception {
+    public static char[] decryptUserPassword(char[] masterPass, String encryptedUserPass, byte[] salt) throws Exception {
         // Convert char[] master password to byte[]
         byte[] masterPassBytes = new String(masterPass).getBytes();
 
-        KeySpec spec = new PBEKeySpec(encryptedPass.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
+        KeySpec masterKeySpec = new PBEKeySpec(encryptedUserPass.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
         SecretKeyFactory factory = SecretKeyFactory.getInstance(SECRET_KEY_ALGORITHM);
-        SecretKey key = factory.generateSecret(spec);
+        SecretKey masterKey = factory.generateSecret(masterKeySpec);
 
         Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decryptedPasswordBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedPass));
+        cipher.init(Cipher.DECRYPT_MODE, masterKey);
+
+        byte[] decryptedPasswordBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedUserPass));
 
         // Convert byte[] decrypted password to char[]
         return new String(decryptedPasswordBytes).toCharArray();
